f38e90c950381c1c571d4c270e56d64ee1fab88a
Valeriy Selitskiy <239034+iamwavecut@users.noreply.github.com>
2024-05-27 17:42:51 +0200

    noop

--- GGUF/util.py
+++ GGUF/util.py
@@ -15,14 +15,15 @@ def run_command(logger: logging.Logger, command: List[str], cwd: str = "."):
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
+        bufsize=1  # Line-buffered
     )
 
     while True:
         output = process.stdout.readline()
-        if output == "" and process.poll() is not None:
-            break
         if output:
             logger.info(output.strip())
+        elif process.poll() is not None:
+            break
 
     stderr_output = process.stderr.read()
     if stderr_output:
